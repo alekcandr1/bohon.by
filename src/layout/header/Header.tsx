7a7018bc6ebf60d94dad0553ab0c21498b21211a
alekcandr1 <alekcandrmain@gmail.com>
2024-01-04 11:38:31 +0300

    add project

---
+++ src/layout/header/Header.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const Header = () => {
+    return (
+        <header>
+            <a href="">
+                <img src="" alt="" />
+            </a>
+            <nav>
+                <li><a href="">Home</a></li>
+                <li><a href="">Skills</a></li>
+                <li><a href="">Projects</a></li>
+                <li><a href="">Testimonials</a></li>
+                <li><a href="">Contacts</a></li>
+            </nav>
+        </header>
+    );
+};
+
+
